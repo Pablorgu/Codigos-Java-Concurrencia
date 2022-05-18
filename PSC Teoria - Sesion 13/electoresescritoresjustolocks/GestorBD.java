d278199a808bef0a27e360c8643b26c6fa0b2b1d
pablorgu <pablorg@uma.es>
2022-05-18 12:29:07 +0200

    final clase 18/2

--- PSC Teoria - Sesion 13/electoresescritoresjustolocks/GestorBD.java
+++ PSC Teoria - Sesion 13/electoresescritoresjustolocks/GestorBD.java
@@ -1,54 +1,73 @@
 package electoresescritoresjustolocks;
 
+import java.lang.Character.UnicodeBlock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class GestorBD {
 
 	private int nLectores = 0;
 	private boolean hayEscritor = false;
 	private int nEscritores = 0;
 
-	private Condicion okLeer = new Condicion();
-	private Condicion okEscribir = new Condicion();
+	private Lock l = new ReentrantLock();
 
-	public synchronized void entraLector(int id) throws InterruptedException {
+	private Condicion okLeer = l.newCondition();
+	private Condicion okEscribir = l.newCondition();
 
-		while (hayEscritor || nEscritores > 0)
-			wait();
+	public void entraLector(int id) throws InterruptedException {
+		l.lock();
+		try{
+			while (hayEscritor || nEscritores > 0)
+				wait();
 
-		nLectores++;
-		System.out.println("Entra lector " + id + ", hay " + nLectores + " lectores");
-
-	}
-
-	public synchronized void saleLector(int id) throws InterruptedException {
-
-		nLectores--;
-
-		System.out.println("Sale lector " + id + ", hay " + nLectores + " lectores");
-
-		if (nLectores == 0)
-			notifyAll();
-
-	}
-
-	public synchronized void entraEscritor(int id) throws InterruptedException {
-
-		nEscritores++;
-		while (nLectores > 0 || hayEscritor) {
-			wait();
+			nLectores++;
+			System.out.println("Entra lector " + id + ", hay " + nLectores + " lectores");
+		}finally{
+			l.unlock();
 		}
+	}
 
-		hayEscritor = true;
+	public void saleLector(int id) throws InterruptedException {
+		l.lock();
+		try{
+			nLectores--;
 
-		System.out.println("                    Entra escritor " + id);
+			System.out.println("Sale lector " + id + ", hay " + nLectores + " lectores");
+
+			if (nLectores == 0)
+				notifyAll();
+		}finally{
+			l.unlock();
+		}
+	}
+
+	public void entraEscritor(int id) throws InterruptedException {
+		l.lock();
+		try{
+			nEscritores++;
+			while (nLectores > 0 || hayEscritor) {
+				wait();
+			}
+
+			hayEscritor = true;
+
+			System.out.println("                    Entra escritor " + id);
+		}finally{
+			l.unlock();
+		}
 	}
 
 	public synchronized void saleEscritor(int id) throws InterruptedException {
-
-		hayEscritor = false;
-		System.out.println("                    Sale escritor " + id);
-		nEscritores--;
-		notifyAll();
-
+		l.lock();
+		try{
+			hayEscritor = false;
+			System.out.println("                    Sale escritor " + id);
+			nEscritores--;
+			notifyAll();
+		}finally {
+			l.unlock();
+		}
 	}
 
 }
